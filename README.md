bc64164c62a9789018586c06459c940399aec190
zdyu <1877682825@qq.com>
2022-10-30 19:52:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# MenuDemoVSIX
-visual studio 插件关于Menu的扩展demo
+工欲善其事,必先利其器，作为程序员我们很大部分时间在和ide打交道，好的插件可以大大提高我们的编程效率，我开发过几个[vs插件](https://marketplace.visualstudio.com/publishers/nainaigu)来解决一键生成dbmodels，快速部署到服务器，总结下来最关键的还是对于Menu这块的扩展，因为这是插件功能的最常见的入口之一，下面给大家介绍vs插件各种menu的扩展
+
+## 文章
+https://www.cnblogs.com/yudongdong/p/16837986.html
---
+++ TestCommand.cs
@@ -0,0 +1,114 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace MenuDemoVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class TestCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("bfd38d96-e3cd-4f50-b857-8167dd432a0e");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        /// <param name="dte">dte.</param>
+        private TestCommand(AsyncPackage package, OleMenuCommandService commandService, DTE2 dte)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+
+            void OnMenuItemOnBeforeQueryStatus(object sender, EventArgs args)
+            {
+                menuItem.Visible = dte.SelectedItems.Item(1).ProjectItem.Name.ToLower().EndsWith(".json");
+            }
+
+            menuItem.BeforeQueryStatus += OnMenuItemOnBeforeQueryStatus;
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static TestCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in TestCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+            var dte = (DTE2)((EnvDTE.DTE)await package.GetServiceAsync(typeof(EnvDTE.DTE)));
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new TestCommand(package, commandService, dte);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            string title = "TestCommand";
+
+            // Show a message box to prove we were here
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
